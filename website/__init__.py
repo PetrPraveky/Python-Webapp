6333a7d80f41762200e4c30f4fea5b7eab1c395d
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-05-09 15:36:41 +0200

    Update 6

--- website/__init__.py
+++ website/__init__.py
@@ -10,23 +10,6 @@ def views_site():
     
     return views
 
-def auth_site():
-    auth = Blueprint('auth', __name__)
-
-    @auth.route("/login")
-    def login():
-        return "<p>Login</p>"
-
-    @auth.route("/logout")
-    def logout():
-        return "<p>Logout</p>"
-
-    @auth.route("/sign-up")
-    def sign_up():
-        return "<p>Sign Up</p>"
-    
-    return auth
-
 
 def create_app():
     with open(os.path.join('website','secret_key.0'), "r", encoding="utf-8") as sc:
@@ -36,6 +19,5 @@ def create_app():
     app.config['SECRET_KEY'] = secret_key
     
     app.register_blueprint(views_site(), url_prefix="/")
-    app.register_blueprint(auth_site(), url_prefix="/")
     
     return app
