a113ff8913adb33219e64c6c4a548d4a27ba1152
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-05-09 11:32:01 +0200

    Update 3

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,32 @@
-from flask import Flask
+from flask import Flask, Blueprint, render_template
 import os
+
+def views_site():
+    views = Blueprint('views', __name__)
+
+    @views.route('/')
+    def home():
+        return render_template('base.html')
+    
+    return views
+
+def auth_site():
+    auth = Blueprint('auth', __name__)
+
+    @auth.route("/login")
+    def login():
+        return "<p>Login</p>"
+
+    @auth.route("/logout")
+    def logout():
+        return "<p>Logout</p>"
+
+    @auth.route("/sign-up")
+    def sign_up():
+        return "<p>Sign Up</p>"
+    
+    return auth
+
 
 def create_app():
     with open(os.path.join('website','secret_key.0'), "r", encoding="utf-8") as sc:
@@ -8,10 +35,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = secret_key
     
-    from .views import views
-    from .auth import auth
-    
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(views_site(), url_prefix="/")
+    app.register_blueprint(auth_site(), url_prefix="/")
     
     return app
