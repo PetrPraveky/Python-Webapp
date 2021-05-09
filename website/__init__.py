180897d990176240b28c7a60c1c33137867405fd
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-05-09 22:30:00 +0200

    Update 12

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,14 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request
 import os
 
 def views_site():
     views = Blueprint('views', __name__)
 
-    @views.route('/')
+    @views.route('/')       
     def home():
         return render_template('home.html')
     
     return views
-
 
 def create_app():
     with open(os.path.join('website','secret_key.0'), "r", encoding="utf-8") as sc:
