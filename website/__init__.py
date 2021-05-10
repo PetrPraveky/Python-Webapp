7d4b00a33942366241549a6751e9409f4a561309
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-05-10 13:22:01 +0200

    Update 13

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,18 @@
-from flask import Flask, Blueprint, render_template, request
+from flask import Flask, Blueprint, render_template, url_for, redirect
 import os
 
 def views_site():
     views = Blueprint('views', __name__)
 
     @views.route('/')       
+    def index():
+        return redirect(url_for('views.home'))
+    @views.route('/home')
     def home():
         return render_template('home.html')
+    @views.route('/info')
+    def info():
+        return render_template('info.html')
     
     return views
 
